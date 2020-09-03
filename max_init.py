925d43543840e4eb89bb3d6f5d7bfa2a07dbe7f9
Berta Rut Harðardóttir <bertarut@Bertas-MacBook-Air.local>
2020-09-03 14:35:34 +0000

    Algorithm written for max_int

---
+++ max_init.py
@@ -0,0 +1,9 @@
+# User puts in a number, find the highest number from the user until he puts in a negative number
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+while num_int >= 0:
+
+
+print("The maximum is", max_int)    # Do not change this line
